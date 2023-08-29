25f38f78c8a4fb62ec5463d1cbd9150ee9575c1e
palucena <palucena@student.42malaga.com>
2023-08-29 17:42:21 +0200

    No idea

---
+++ includes/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 17:29:21 by palucena          #+#    #+#             */
+/*   Updated: 2023/08/29 17:36:07 by palucena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSHSWAP_H
+# define PUSHSWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "lib/libft.h"
+
+// Estructuras?
+
+// Funciones?
+
+#endif
