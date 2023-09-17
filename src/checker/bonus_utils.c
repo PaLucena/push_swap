63d71c447a22c6bb2d163ef2f07393c35b6f8a54
palucena <palucena@student.42malaga.com>
2023-09-17 19:06:43 +0200

    Fixed checker

--- src/checker/bonus_utils.c
+++ src/checker/bonus_utils.c
@@ -6,7 +6,7 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:04:49 by palucena          #+#    #+#             */
-/*   Updated: 2023/09/11 15:36:27 by palucena         ###   ########.fr       */
+/*   Updated: 2023/09/17 18:49:31 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,30 @@
 
 void	ft_choose_mov(t_list **a, t_list **b, char *str)
 {
-	if (ft_strncmp(str, "pa", 2) == 0)
+	if (ft_strncmp(str, "pa\n", 3) == 0)
 		ft_push(b, a, 'a', false);
-	else if (ft_strncmp(str, "pb", 2) == 0)
+	else if (ft_strncmp(str, "pb\n", 3) == 0)
 		ft_push(a, b, 'b', false);
-	else if (ft_strncmp(str, "sa", 2) == 0)
+	else if (ft_strncmp(str, "sa\n", 3) == 0)
 		ft_swap(a, b, 'a', false);
-	else if (ft_strncmp(str, "sb", 2) == 0)
+	else if (ft_strncmp(str, "sb\n", 3) == 0)
 		ft_swap(a, b, 'b', false);
-	else if (ft_strncmp(str, "ss", 2) == 0)
+	else if (ft_strncmp(str, "ss\n", 3) == 0)
 		ft_swap(a, b, 's', false);
-	else if (ft_strncmp(str, "rra", 3) == 0)
+	else if (ft_strncmp(str, "rra\n", 4) == 0)
 		ft_reverse_rotate(a, b, 'a', false);
-	else if (ft_strncmp(str, "rrb", 3) == 0)
+	else if (ft_strncmp(str, "rrb\n", 4) == 0)
 		ft_reverse_rotate(a, b, 'b', false);
-	else if (ft_strncmp(str, "rrr", 3) == 0)
+	else if (ft_strncmp(str, "rrr\n", 4) == 0)
 		ft_reverse_rotate(a, b, 'r', false);
-	else if (ft_strncmp(str, "ra", 2) == 0)
+	else if (ft_strncmp(str, "ra\n", 3) == 0)
 		ft_rotate(a, b, 'a', false);
-	else if (ft_strncmp(str, "rb", 2) == 0)
+	else if (ft_strncmp(str, "rb\n", 3) == 0)
 		ft_rotate(a, b, 'b', false);
-	else if (ft_strncmp(str, "rr", 2) == 0)
+	else if (ft_strncmp(str, "rr\n", 3) == 0)
 		ft_rotate(a, b, 'r', false);
+	else
+		error_msg(1);
 }
 
 void	ft_apply(t_list **a, t_list **b)
